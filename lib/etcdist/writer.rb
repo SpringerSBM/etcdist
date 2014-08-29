c664d653955be9b8ea91a4299499983cd676c4db
Hilverd Reker <me@hilverd.com>
2014-08-29 15:30:43 +0100

    Pat/Hilverd: Join delete/write into a single method (which fixes a possible bug).

--- lib/etcdist/writer.rb
+++ lib/etcdist/writer.rb
@@ -11,24 +11,16 @@ module Etcdist
     end
 
     def write(data)
-      delete(data)
-      set(data)
-    end
-
-    private
-    def set(data)
       data.each do |directory, entries|
+
+        keys = @etcd.get(directory).children.map { |n| n.key.sub(/.*\//,'') }
+        to_delete = keys - entries.keys
+
         entries.each do |k, v|
           @etcd.set([directory, '/', k].join, value: v)
         end
         Log.info("wrote #{entries.length} entries to #{directory}")
-      end
-    end
 
-    def delete(data)
-      data.each do |directory, entries|
-        keys = @etcd.get(directory).children.map { |n| n.key.sub(/.*\//,'') }
-        to_delete = keys - entries.keys
         to_delete.each { |k| @etcd.delete([directory, '/', k].join) }
         Log.info("deleted #{to_delete.length} entries from #{directory}")
       end
